550db95b4f2dcf2a1968614ab77c3f2aef631728
Félicien Corbat <felicien@champis.net>
2022-03-24 11:06:34 +0100

    Add errors for negative trunks ages

--- functions/export_functions/trunks_errors.py
+++ functions/export_functions/trunks_errors.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pandas as pd
 
 
@@ -18,5 +20,14 @@ def trunks_errors(data: pd.DataFrame) -> pd.DataFrame:
     species_trunk_errors["Type d'erreur"] = "Incohérence dans l'espèce du tronc"
     species_trunk_errors["Ligne"] = species_trunk_errors.index + 2
     errors = pd.concat([errors, species_trunk_errors])
+
+    # Erreurs: Age du tronc négatif
+    age_trunk_errors = data[(data['Age du tronc'] < datetime.timedelta(seconds=0))]
+    age_trunk_errors = age_trunk_errors[['Substrat']]
+    age_trunk_errors["Type d'erreur"] = "L'âge est négatif pour le tronc " + age_trunk_errors['Substrat']
+    age_trunk_errors["Espèce"] = ""
+    age_trunk_errors["Ligne"] = age_trunk_errors.index + 2
+    age_trunk_errors = age_trunk_errors[["Espèce", "Type d'erreur", "Ligne"]]
+    errors = pd.concat([errors, age_trunk_errors])
 
     return errors
