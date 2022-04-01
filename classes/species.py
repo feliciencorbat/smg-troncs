ce03f2ff825e13b8576bdb837ec8386d2a9f0fae
Félicien Corbat <felicien@champis.net>
2022-04-01 14:31:46 +0200

    Improve species class with property

--- classes/species.py
+++ classes/species.py
@@ -1,25 +1,23 @@
 class Species:
-    def __init__(self, species, phylum, order, rank):
-        self.species = correction_canonical_name_var(species, rank)
+    def __init__(self, species: str, phylum: str, order: str, rank: str):
+        self._species = species
         self.phylum = phylum
         self.order = order
         self.rank = rank
 
+    @property
+    def species(self):
+        # Corriger canonical name pour les variétés, formes et sous-espèces
+        if self.rank == "VARIETY" or self.rank == "SUBSPECIES" or self.rank == "FORM":
 
-def correction_canonical_name_var(species, rank) -> str:
-    # Corriger canonical name pour les variétés, formes et sous-espèces
-    if rank == "VARIETY" or rank == "SUBSPECIES" or rank == "FORM":
+            split_canonical_name = self._species.split(" ")
+            if self.rank == "VARIETY":
+                term = "var."
+            elif self.rank == "FORM":
+                term = "fo."
+            else:
+                term = "subsp."
 
-        split_canonical_name = species.split(" ")
-        if rank == "VARIETY":
-            term = "var."
-        elif rank == "FORM":
-            term = "fo."
+            return split_canonical_name[0] + " " + split_canonical_name[1] + " " + term + " " + split_canonical_name[2]
         else:
-            term = "subsp."
-
-        return split_canonical_name[0] + " " + split_canonical_name[1] \
-               + " " + term + " " + split_canonical_name[2]
-
-    else:
-        return species
+            return self._species
