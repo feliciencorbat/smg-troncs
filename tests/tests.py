c366bbd9dc89070923fd69eb31688a93fe66c901
Félicien Corbat <felicien@champis.net>
2022-04-02 16:46:58 +0200

    Correction tests only with GBIF

--- tests/tests.py
+++ tests/tests.py
@@ -14,29 +14,8 @@ from functions.nb_species_evolution import nb_species_evolution
 class Tests(unittest.TestCase):
 
     def test_functions(self):
-        print("Test de l'export sans GBIF")
-        export("liste_originale.xls", False)
-
-        data = pd.read_excel("export/liste_modifiee.xlsx", sheet_name="Statistiques")
-
-        print("Test de la fonction de distribution")
-        distribution_bar(data, "Espèce", "Titre du graphe", 20, True, "Tous les lieux")
-
-        print("Test matrice Cramer")
-        cramer_matrix(data, True, "Tous les lieux")
-
-        print("Test de la fonction chi 2")
-        chi2_test(data, "Espèce du tronc", "Menace", "Titre du graphe", False,
-                  True, "Tous les lieux")
-
-        print("Test de la fonction ANOVA")
-        anova_test(data, "Menace", "Age du tronc", "Titre du graphe", True, "Tous les lieux")
-
-        print("Test de la fonction évolution espèces")
-        nb_species_evolution(data, True, "Tous les lieux")
-
-        print("Test de l'export avec GBIF")
-        export("liste_originale.xls", True)
+        print("Test de l'export")
+        export("liste_originale.xls")
 
         data = pd.read_excel("export/liste_modifiee.xlsx", sheet_name="Statistiques")
 
