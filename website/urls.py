cfead8972418509c605596528ecc93fed1a0e1bb
Félicien Corbat <felicien@champis.net>
2022-04-23 04:41:46 +0200

    Add home page

--- website/urls.py
+++ website/urls.py
@@ -17,8 +17,11 @@ import authentication.views
 from django.contrib import admin
 from django.urls import path
 
+import stats.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', authentication.views.login_page, name='login'),
     path('logout/', authentication.views.logout_user, name='logout'),
+    path('home/', stats.views.home, name='home'),
 ]
