af3885e4f135df36c224940bf28ef865f5955532
Félicien Corbat <felicien@champis.net>
2022-04-25 14:14:53 +0200

    Debug docker-compose.yml

--- website/settings.py
+++ website/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'django-insecure-x5&(bg0=ukg13+xs)_z1(mw%=nnr7@f^3&rp=5%$wn(sz^mc%*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
