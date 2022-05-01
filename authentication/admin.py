df3ae5f64d5cb96db8fd3085c1300080f3abae4d
Félicien Corbat <felicien@champis.net>
2022-05-01 17:49:02 +0200

    Improve user admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff']
+
+admin.site.register(User, CustomUserAdmin)
