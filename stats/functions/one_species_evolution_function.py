d8aa3d0d8c414838120d3686e67bdf2bc0348615
Félicien Corbat <felicien@champis.net>
2022-05-03 23:01:17 +0200

    Add last date line (if absent) in one species evolution

--- stats/functions/one_species_evolution_function.py
+++ stats/functions/one_species_evolution_function.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import matplotlib
 import numpy as np
@@ -17,6 +18,11 @@ def one_species_evolution_function(data: pd.DataFrame, species: str, with_cf: bo
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+    year = data.iloc[-1]["Date"].year
+    month = data.iloc[-1]["Date"].month
+    last_date = datetime.datetime(year=year, month=month, day=28)
+
+
     # Filtrer espèce
     data = data.loc[data["Espèce"] == species]
 
@@ -32,7 +38,7 @@ def one_species_evolution_function(data: pd.DataFrame, species: str, with_cf: bo
     writer = pd.ExcelWriter(directory + '/one_species_evolution.xlsx', engine='xlsxwriter', datetime_format='dd.mm.yyyyy', date_format='dd.mm.yyyyy')
 
     # Création du graphique cumulation de l'espèce
-    total_obs = cumulative_observations(data)
+    total_obs = cumulative_observations(data, last_date)
     total_obs.to_excel(writer, sheet_name='Toutes les observations')
     plt.figure()
     plt.plot(total_obs.index, total_obs["Observations cumulées"],
@@ -48,7 +54,7 @@ def one_species_evolution_function(data: pd.DataFrame, species: str, with_cf: bo
     plt.figure()
     for trunk in data["Tronc"].dropna().unique():
         trunk_obs = data.loc[(data["Tronc"] == trunk)]
-        trunk_obs = cumulative_observations(trunk_obs)
+        trunk_obs = cumulative_observations(trunk_obs, last_date)
         if len(trunk_obs) > 0:
             plt.plot(trunk_obs.index, trunk_obs["Observations cumulées"],
                      label="Tronc " + str(trunk))
@@ -64,7 +70,7 @@ def one_species_evolution_function(data: pd.DataFrame, species: str, with_cf: bo
     plt.figure()
     for trunk_group in data["Groupe troncs"].dropna().unique():
         trunk_group_obs = data.loc[(data["Groupe troncs"] == trunk_group)]
-        trunk_group_obs = cumulative_observations(trunk_group_obs)
+        trunk_group_obs = cumulative_observations(trunk_group_obs, last_date)
         if len(trunk_group_obs) > 0:
             plt.plot(trunk_group_obs.index, trunk_group_obs["Observations cumulées"],
                      label="Groupe de troncs " + str(trunk_group))
@@ -80,10 +86,10 @@ def one_species_evolution_function(data: pd.DataFrame, species: str, with_cf: bo
     plt.figure()
     for trunk_species in data["Espèce du tronc"].dropna().unique():
         trunk_species_obs = data.loc[(data["Espèce du tronc"] == trunk_species)]
-        trunk_species_obs = cumulative_observations(trunk_species_obs)
+        trunk_species_obs = cumulative_observations(trunk_species_obs, last_date)
         if len(trunk_species_obs) > 0:
             plt.plot(trunk_species_obs.index, trunk_species_obs["Observations cumulées"],
-                     label="Sur " + str(trunk_species))
+                     label=str(trunk_species))
             trunk_species_obs.to_excel(writer, sheet_name="Observations sur " + trunk_species)
 
     plt.ylabel("Nombre d'observations")
@@ -94,9 +100,20 @@ def one_species_evolution_function(data: pd.DataFrame, species: str, with_cf: bo
 
     writer.save()
 
-def cumulative_observations(data: pd.DataFrame) -> pd.DataFrame:
+def cumulative_observations(data: pd.DataFrame, last_date) -> pd.DataFrame:
     count = data[["Date", "Espèce"]].groupby(pd.Grouper(key='Date', freq='1M')).count()
     observations = pd.DataFrame([], )
-    observations["Obervations"] = count["Espèce"]
+    observations["Observations"] = count["Espèce"]
     observations["Observations cumulées"] = observations.cumsum()
+    observations_last_date = observations.iloc[-1].name
+
+
+    # Ajouter une ligne de la dernière date du dataframe total si absente
+    if len(observations) > 0:
+        if observations_last_date < last_date:
+            new_row = observations.iloc[-1:]
+            new_row = new_row.rename(index={observations_last_date: last_date})
+            new_row['Observations'] = 0
+            observations = pd.concat([observations, new_row])
+
     return observations
