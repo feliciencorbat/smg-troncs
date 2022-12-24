c15fd6bbf997d18c30ccd6a99f954ceb889d9924
Félicien Corbat <felicien@champis.net>
2022-12-24 08:31:16 +0100

    Add titles in report

--- stats/functions/report/docx_generation.py
+++ stats/functions/report/docx_generation.py
@@ -18,21 +18,39 @@ def docx_generation(new_species_maillettes, new_species_bossy, new_species_isole
     document.add_paragraph('De nombreux champignons de la liste rouge des espèces menacées en Suisse croissent sur de vieux arbres morts, de grands diamètres, principalement de feuillus. De tels arbres sont rarement rencontrés en forêt et par conséquent une niche écologique manque souvent pour ces espèces. Afin de créer un biotope favorable pour ces champignons, des troncs de feuillus de grands diamètres (chênes et peupliers principalement, mais aussi de saule et de marronniers) ont été déposés au sol, en bordure d’une route, en lisière de forêt, sur un sol humide.')
     document.add_paragraph('Un suivi mycologique régulier a été entrepris dès 2014.')
 
+    document.add_paragraph('')
+
+    material_title = document.add_paragraph('')
+    material_title.add_run('Matériel et méthode :').bold = True
+
+    document.add_paragraph('')
+
+    result_title = document.add_paragraph('')
+    result_title.add_run('Résultat ').bold = True
+    result_title.add_run('(au 31.12.' + year + ')')
+
+    document.add_paragraph('')
+
+    material_title = document.add_paragraph('')
+    material_title.add_run('Discussion :').bold = True
+
+    document.add_paragraph('')
+
     annexes = document.add_paragraph('')
     annexes.add_run('Annexes :').bold = True
 
-    document.add_paragraph('Tableau 1 : Nouvelles espèces trouvées en ' + year + ' sur les troncs du chemin des Maillettes')
-    document.add_paragraph('Tableau 2 : Nouvelles espèces trouvées en ' + year + ' sur les troncs du chemin de Bossy')
-    document.add_paragraph('Tableau 3 : Nouvelles espèces trouvées en ' + year + ' sur le tronc SMG')
-    document.add_paragraph('Tableau 4 : Espèces de la liste rouge, rares ou assez rares trouvées en ' + year + ' sur les troncs du chemin des Maillettes')
-    document.add_paragraph('Tableau 5 : Espèces de la liste rouge, rares ou assez rares trouvées en ' + year + ' sur les troncs du chemin de Bossy')
-    document.add_paragraph('Tableau 6 : Espèces de la liste rouge, rares ou assez rares trouvées en ' + year + ' sur le tronc SMG')
+    document.add_paragraph('Tableau 1 : ' + str(new_species_maillettes.shape[0]) + ' nouvelles espèces trouvées en ' + year + ' sur les troncs du chemin des Maillettes')
+    document.add_paragraph('Tableau 2 : ' + str(new_species_bossy.shape[0]) + ' nouvelles espèces trouvées en ' + year + ' sur les troncs du chemin du Pont-de-Bossy')
+    document.add_paragraph('Tableau 3 : ' + str(new_species_isole.shape[0]) + ' nouvelles espèces trouvées en ' + year + ' sur le tronc SMG du chemin du Pont-de-Bossy')
+    document.add_paragraph('Tableau 4 : ' + str(rare_species_maillettes.shape[0]) + ' espèces de la liste rouge, rares ou assez rares trouvées en ' + year + ' sur les troncs du chemin des Maillettes')
+    document.add_paragraph('Tableau 5 : ' + str(rare_species_bossy.shape[0]) + ' espèces de la liste rouge, rares ou assez rares trouvées en ' + year + ' sur les troncs du chemin du Pont-de-Bossy')
+    document.add_paragraph('Tableau 6 : ' + str(rare_species_isole.shape[0]) + ' espèces de la liste rouge, rares ou assez rares trouvées en ' + year + ' sur le tronc SMG du chemin du Pont-de-Bossy')
 
     document.add_page_break()
 
     annexe1 = document.add_paragraph('')
     annexe1.add_run(
-        'Tableau 1 : Nouvelles espèces trouvées en ' + year + ' sur les troncs du chemin des Maillettes').bold = True
+        'Tableau 1 : ' + str(new_species_maillettes.shape[0]) + ' nouvelles espèces trouvées en ' + year + ' sur les troncs du chemin des Maillettes').bold = True
 
     table = document.add_table(rows=1, cols=3)
     hdr_cells = table.rows[0].cells
@@ -51,7 +69,7 @@ def docx_generation(new_species_maillettes, new_species_bossy, new_species_isole
 
     annexe2 = document.add_paragraph('')
     annexe2.add_run(
-        'Tableau 2 : Nouvelles espèces trouvées en ' + year + ' sur les troncs du chemin de Bossy').bold = True
+        'Tableau 2 : ' + str(new_species_bossy.shape[0]) + ' nouvelles espèces trouvées en ' + year + ' sur les troncs du chemin du Pont-de-Bossy').bold = True
 
     table = document.add_table(rows=1, cols=3)
     hdr_cells = table.rows[0].cells
@@ -70,7 +88,7 @@ def docx_generation(new_species_maillettes, new_species_bossy, new_species_isole
 
     annexe3 = document.add_paragraph('')
     annexe3.add_run(
-        'Tableau 3 : Nouvelles espèces trouvées en ' + year + ' sur le tronc SMG').bold = True
+        'Tableau 3 : ' + str(new_species_isole.shape[0]) + ' nouvelles espèces trouvées en ' + year + ' sur le tronc SMG du chemin du Pont-de-Bossy').bold = True
 
     table = document.add_table(rows=1, cols=2)
     hdr_cells = table.rows[0].cells
@@ -86,7 +104,7 @@ def docx_generation(new_species_maillettes, new_species_bossy, new_species_isole
     document.add_page_break()
 
     annexe4 = document.add_paragraph('')
-    annexe4.add_run('Tableau 4 : Espèces de la liste rouge, rares ou assez rares trouvées en ' + year + ' sur les troncs du chemin des Maillettes').bold = True
+    annexe4.add_run('Tableau 4 : ' + str(rare_species_maillettes.shape[0]) + ' espèces de la liste rouge, rares ou assez rares trouvées en ' + year + ' sur les troncs du chemin des Maillettes').bold = True
 
     table = document.add_table(rows=1, cols=3)
     hdr_cells = table.rows[0].cells
@@ -105,7 +123,7 @@ def docx_generation(new_species_maillettes, new_species_bossy, new_species_isole
 
     annexe5 = document.add_paragraph('')
     annexe5.add_run(
-        'Tableau 5 : Espèces de la liste rouge, rares ou assez rares trouvées en ' + year + ' sur les troncs du chemin de Bossy').bold = True
+        'Tableau 5 : ' + str(rare_species_bossy.shape[0]) + ' espèces de la liste rouge, rares ou assez rares trouvées en ' + year + ' sur les troncs du chemin du Pont-de-Bossy').bold = True
 
     table = document.add_table(rows=1, cols=3)
     hdr_cells = table.rows[0].cells
@@ -124,7 +142,7 @@ def docx_generation(new_species_maillettes, new_species_bossy, new_species_isole
 
     annexe6 = document.add_paragraph('')
     annexe6.add_run(
-        'Tableau 6 : Espèces de la liste rouge, rares ou assez rares trouvées en ' + year + ' sur le tronc SMG').bold = True
+        'Tableau 6 : ' + str(rare_species_isole.shape[0]) + ' espèces de la liste rouge, rares ou assez rares trouvées en ' + year + ' sur le tronc SMG du chemin du Pont-de-Bossy').bold = True
 
     table = document.add_table(rows=1, cols=2)
     hdr_cells = table.rows[0].cells
